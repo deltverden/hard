b22283277685477d0c221a7079a013124fcb5d73
deltverden <d.bilets@edu.internetdevels.com>
2020-07-03 13:32:08 +0300

    Created slider.js

---
+++ assets/js/slider.js
@@ -0,0 +1,12 @@
+'use strict';
+
+$('.sl-js').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 1000,
+    infinite: false,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 4000
+});
